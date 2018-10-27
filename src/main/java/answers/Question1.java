8ac6e11f50a4aa4e5b8773096c5d9e2c96d60c74
Dullaz <dulla2go@gmail.com>
2018-10-27 16:26:26 +0100

    Update Question1.java

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,16 +3,26 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		int maxm = 0;
-		for(int i =0;i<portfolios.length();i++)
-		{
-			for(int j=i+1;j<portfolios.length();j++)
-			{
-				maxm = Math.max(portfolios[i]^portfolios[j],maxm);
-			}
-		}
-		return maxm;
-	}
 
+		// TODO Auto-generated method stub
+
+		int maxm = 0;
+
+		for(int i =0;i<portfolios.length;i++)
+
+		{
+
+			for(int j=i+1;j<portfolios.length;j++)
+
+			{
+
+				maxm = Math.max(portfolios[i]^portfolios[j],maxm);
+
+			}
+
+		}
+
+		return maxm;
+
+	}
 }
