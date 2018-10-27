d64c0451eb2795361d54063580f9ae9fd520feb3
Dullaz <dulla2go@gmail.com>
2018-10-27 16:53:44 +0100

    Update Question4.java

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -2,9 +2,33 @@ package answers;
 
 public class Question4 {
 
-	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return -1;
-	}
+    public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
+        // TODO Auto-generated method stub
+        int mintime = Integer.MAX_VALUE;
+        int j = rows[0].length - numberMachines;
+        int tempmin = 0;
+        for (String[] row : rows) {
+            System.out.println(Arrays.toString(row));
+            for (int i = 0; i <= j; i++) {
+                tempmin = 0;
+                for (int k = 0; k < numberMachines; k++) {
+                    try {
+                        tempmin += Integer.parseInt(row[i + k]);
+                    } catch (Exception e) {
+                        tempmin = 0;
+                    }
+                }
+                if (tempmin != 0) {
+                    mintime = Math.min(mintime, tempmin);
+                }
+            }
+        }
+        return mintime;
+    }
+
+    public static void main(String[] args) {
+        String[][] arr = { { "1", "2", "3", "4" }, { "1", "X", "2", "X" }, { "1", "X", "1", "1" } };
+        System.out.println(selectionFailedTradedesks(arr, 4));
+    }
 
 }
